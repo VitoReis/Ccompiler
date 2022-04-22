a5294afc5159d7e92ef5effe541f809ea4eb5aa8
Vitor Silva Reis <84536221+VitoReis@users.noreply.github.com>
2022-04-21 21:08:02 -0300

    Compilador
    Código fonte do compilador.

---
+++ code.c
@@ -0,0 +1,22 @@
+void main(){
+    printf("Hello World");
+    printf("");
+    int a, b;
+    a = 2;
+    float c;
+    b = 6;
+    float ab = 2;
+    ab *= 69.8;
+    c = a + b;
+    c --;
+    c++;
+    c = c / 2;
+    printf ("%f", c );
+    for(int i=0; i < 10; i++){
+        c *= i;
+    }
+    while(true){
+        printf("Hello Friend");
+	    break;
+    }
+}
---
+++ compiler.py
@@ -0,0 +1,3 @@
+from lexicalAnalyzer import *
+
+analyse()
